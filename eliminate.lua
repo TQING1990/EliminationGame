1208a1112bc9982b19b2d24f5d01ace9293e6dbd
TQING <tqing1128@gmail.com>
2019-09-09 10:20:20 +0800

    完善

--- eliminate.lua
+++ eliminate.lua
@@ -1,40 +1,36 @@
 
+-- 消除方向: 横向和纵向
+local Direction = {
+    HORIZONTAL = { tag = "h", x = -1, y = 0 },
+    VERTICAL = { tag = "v", x = 0, y = 1 },
+}
+
+-- 用于递归获取连续相同颜色的点
+-- 正向
+local POSITIVE = 1
+-- 反向
+local REVERSE = -1
+
+-- 消除最小数量
+local ELIMINATE_MIN_COUNT = 3
+
 local function log(fmt, ...)
     print(string.format(fmt, ...))
-end
-
-local function convertMatrix(config)
-    local matrix = {}
-    for y, xl in ipairs(config) do
-        local t = {}
-        matrix[y] = t
-        for x, color in ipairs(xl) do
-            t[x] = { x = x, y = y, color = color }
-        end
-    end
-    return matrix
 end
 
 local function getPoint(matrix, x, y)
     return matrix[y] and matrix[y][x]
 end
 
-
-local Direction = {
-    HORIZONTAL = { tag = "h", x = -1, y = 0 },
-    VERTICAL = { tag = "v", x = 0, y = 1 },
-}
-
-local DirectionList = { Direction.HORIZONTAL, Direction.VERTICAL }
-
-local function nextContinuousPoint(matrix, ret, point, direction, x, y)
+local function nextContinuousPoint(matrix, ret, point, direction, positiveOrReverse)
+    local x, y = direction.x * positiveOrReverse, direction.y * positiveOrReverse
     log("(%s,%s)->(%s,%s)", point.x, point.y, point.x + x, point.y + y)
     local nextPoint = getPoint(matrix, point.x + x, point.y + y)
     if nextPoint then
         if nextPoint.color == point.color then
             if nil == nextPoint[direction.tag] then
                 table.insert(ret, nextPoint)
-                nextContinuousPoint(matrix, ret, nextPoint, direction, x, y)
+                nextContinuousPoint(matrix, ret, nextPoint, direction, positiveOrReverse)
             else
                 return false
             end
@@ -50,12 +46,11 @@ end
 
 local function getContinuousPoints(matrix, point, direction)
     local ret = {}
-    local x, y = direction.x, direction.y
     log("\ngetContinuousPoints point(%s,%s)", point.x, point.y)
-    log("\ndirection(%s:%s,%s)", direction.tag, direction.x, direction.y)
-    nextContinuousPoint(matrix, ret, point, direction, x, y)
-    log("\ndirection(%s:%s,%s)", direction.tag, -direction.x, -direction.y)
-    nextContinuousPoint(matrix, ret, point, direction, -x, -y)
+    log("\ndirection(%s:%s,%s)", direction.tag, direction.x * POSITIVE, direction.y * POSITIVE)
+    nextContinuousPoint(matrix, ret, point, direction, POSITIVE)
+    log("\ndirection(%s:%s,%s)", direction.tag, direction.x * REVERSE, direction.y * REVERSE)
+    nextContinuousPoint(matrix, ret, point, direction, REVERSE)
     if next(ret) then
         return ret
     else
@@ -65,21 +60,28 @@ local function getContinuousPoints(matrix, point, direction)
 end
 
 local function add(ret, point)
-    local x, y = point.x, point.y
-    ret[y] = ret[y] or {}
-    ret[y][x] = point
+    if not point.mark then
+        local x, y = point.x, point.y
+        ret[y] = ret[y] or {}
+        ret[y][x] = point
+        point.mark = true
+    end
 end
 
+local DirectionList = { Direction.HORIZONTAL, Direction.VERTICAL }
 local function eliminate(matrix, ret, point)
     for _, direction in pairs(DirectionList) do
         local tag = direction.tag
         if nil == point[direction.tag] then
             local points = getContinuousPoints(matrix, point, direction)
             if points then
-                if #points >= 2 then
+                -- 连续的点没有包括自己，所以要减 1
+                if #points >= ELIMINATE_MIN_COUNT - 1 then
+                    -- 把自己加入到结果中，并且标记这个方向已经ok
                     add(ret, point)
                     point[tag] = true
 
+                    -- 把其他点加入到结果中，并且标记这个方向已经ok
                     for i, p in ipairs(points) do
                         add(ret, p)
                         p[tag] = true
@@ -100,6 +102,34 @@ local function eliminate(matrix, ret, point)
     end
 end
 
+--[[
+    将配置转换为矩阵
+    为了直观，将配置的表转换为下面这样的坐标系
+    {
+       ^
+       |  {1, 1, 1, 1, 1, 1, 1},
+       |  {1, 2, 2, 2, 1, 1, 1},
+       |  {1, 1, 1, 1, 1, 1, 1},
+       |  {1, 2, 2, 1, 1, 1, 1},
+       |  {1, 1, 1, 1, 1, 1, 1},
+       ————————————————————————————>
+    }
+ 
+]]
+local function convertMatrix(config)
+    local matrix = {}
+    local ySize = #config
+    for i, xl in ipairs(config) do
+        local y = ySize - i + 1
+        local t = {}
+        matrix[y] = t
+        for x, color in ipairs(xl) do
+            t[x] = { x = x, y = y, color = color }
+        end
+    end
+    return matrix
+end
+
 local matrix = convertMatrix {
     {1, 1, 1, 1, 1, 1, 1},
     {1, 2, 2, 2, 1, 1, 1},
@@ -108,11 +138,14 @@ local matrix = convertMatrix {
     {1, 1, 1, 1, 1, 1, 1},
 }
 
-local function dump(ret)
-    for y = 1, 5 do
+local ret = {}
+eliminate(matrix, ret, getPoint(matrix, 2, 4))
+
+local function dump(result)
+    for y = 5, 1, -1 do
         local t = {}
         for x = 1, 7 do
-            if ret[y] and ret[y][x] then
+            if result[y] and result[y][x] then
                 table.insert(t, "o")
             else
                 table.insert(t, "x")
@@ -121,8 +154,5 @@ local function dump(ret)
         log(table.concat(t, " "))
     end
 end
-
-local ret = {}
-eliminate(matrix, ret, matrix[2][2])
 
 dump(ret)
